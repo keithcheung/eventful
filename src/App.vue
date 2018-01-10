0c74a4a0c66b2d712ec8a0c061735768617c4bc2
keithcheung <keith.cheung41@gmail.com>
2018-01-10 00:13:22 -0500

    new buttons

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
       <v-toolbar-title>Cars and Coffee</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>view_list</v-icon>
-          View Meetup
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{item.icon}}</v-icon>
+          {{item.title}}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -17,12 +17,12 @@
       v-model="sideNav"
       temporary>
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
           <v-list-tile-action>
-            <v-icon>view_list</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            View Meetup
+            {{ item.title }}
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -37,7 +37,15 @@
   export default {
     data () {
       return {
-        sideNav: false
+        sideNav: false,
+        menuItems: [
+          { icon: 'view_list', title: 'View Meetup' },
+          { icon: 'room', title: 'Organize Meetup' },
+          { icon: 'person', title: 'Profile' },
+          // Unauth shouldn't have a sign in or profile
+          { icon: 'face', title: 'Sign Up' },
+          { icon: 'lock_open', title: 'Sign In' }
+        ]
       }
     }
   }
