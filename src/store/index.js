fc378f6487169f3c879b520f0497982c175b214d
keithcheung <keith.cheung41@gmail.com>
2018-08-28 23:46:48 -0400

    Sign In Implemented

--- src/store/index.js
+++ src/store/index.js
@@ -71,6 +71,23 @@ export const store = new Vuex.Store({
             console.log(error)
           }
         )
+    },
+    signUserIn ({commit}, payload) {
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          user => {
+            const newUser = {
+              id: user.user.uid,
+              registeredEvents: []
+            }
+            commit('setUser', newUser)
+          }
+        )
+        .catch(
+          error => {
+            console.log(error)
+          }
+        )
     }
   },
   getters: {
