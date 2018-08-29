47fb2ca77f66fefb8c929b784837148b2db97352
keithcheung <keith.cheung41@gmail.com>
2018-08-28 23:05:19 -0400

    Created Base Sign Up form for User and init Firebase in main component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
-
+import * as firebase from 'firebase'
 import App from './App'
 import router from './router'
 
@@ -21,5 +21,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBhiS0gMDC0_XUL-cTOZnkPVK71eCwdWgQ',
+      authDomain: 'eventful-ffba5.firebaseapp.com',
+      databaseURL: 'https://eventful-ffba5.firebaseio.com',
+      projectId: 'eventful-ffba5',
+      storageBucket: 'eventful-ffba5.appspot.com'
+    })
+  }
 })
